2c1154167c08ec8ec6af7c7acc377826f59c33f3
JP <38791692+SkullDude1994@users.noreply.github.com>
2022-07-13 15:26:25 +0200

    added block comments

--- main.ts
+++ main.ts
@@ -1,5 +1,11 @@
 namespace EvenNumberChecker{
 
+/**
+ *  Checks if a number is even
+ */
+
+//% blockID = CheckIfANumberIsEven
+//% block = "Check if number is even"
     export function CheckEven(num:number):boolean
     {
         const result = !! (num % 2);
